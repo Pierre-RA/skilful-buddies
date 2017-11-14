a2625c4b33d462a8656cedbd552a44a6d9b79b66
Pierre REPETTO-ANDIPATIN <pierre@anthillsolutions.ch>
2017-11-14 16:22:00 +0100

    feat: link chat to service

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -12,12 +12,20 @@ export class ChatComponent implements OnInit {
 
   chats: Array<Chat>;
   current: Chat;
+  active: number;
 
   constructor(
     private chatService: ChatService
   ) {
+    this.active = -1;
     this.chatService.getHeaders().subscribe(chats => {
       this.chats = chats;
+      if (this.chats.length > 0) {
+        this.active = 0;
+        this.chatService.getContent(0).subscribe(chat => {
+          this.current = chat;
+        });
+      }
     });
   }
 
