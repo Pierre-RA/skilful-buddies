fc8b74a37cb7298091b6ccd16eae23be6b6634dd
Pierre Repetto-Andipatin <pierre@anthillsolutions.ch>
2017-12-01 07:16:59 +0100

    Delete environment.prod.ts

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  apiBase: 'https://skilfulbuddies.anthillsolutions.net/api',
-  mapsAPI: 'AIzaSyDRiIvpd4VwvHRz2MJJlBILCv3vVuzpUZg',
-  chatBase: 'https://skilfulbuddies.anthillsolutions.net/api',
-  facebookAppId: '1465809513539908',
-  facebookVersion: 'v2.11'
-};
